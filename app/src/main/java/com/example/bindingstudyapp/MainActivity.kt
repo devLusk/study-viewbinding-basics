55d4c19bb725e272e2f523f401f48ee639dce0a0
Lucas <lucasvida2005@gmail.com>
2025-10-19 23:07:41 -0300

    feat: implement button click listener

--- app/src/main/java/com/example/bindingstudyapp/MainActivity.kt
+++ app/src/main/java/com/example/bindingstudyapp/MainActivity.kt
@@ -1,10 +1,15 @@
 package com.example.bindingstudyapp
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.lifecycle.lifecycleScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,6 +20,18 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        val button = findViewById<Button>(R.id.btnChangeText)
+        val textView = findViewById<TextView>(R.id.tvMessage)
+
+        val mainText = textView.text
+        button.setOnClickListener {
+            lifecycleScope.launch {
+                textView.text = "Text changed with findViewById"
+                delay(3000) // coroutines suspend function
+                textView.text = mainText
+            }
         }
     }
 }
