f7a3a9f6694efa1dbea0d84f837f890222988292
Lucas <lucasvida2005@gmail.com>
2025-10-19 23:30:01 -0300

    refactor: replace findViewById with ViewBinding in MainActivity

--- app/src/main/java/com/example/bindingstudyapp/MainActivity.kt
+++ app/src/main/java/com/example/bindingstudyapp/MainActivity.kt
@@ -1,38 +1,32 @@
 package com.example.bindingstudyapp
 
+import android.annotation.SuppressLint
 import android.os.Bundle
-import android.widget.Button
-import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import androidx.lifecycle.lifecycleScope
+import com.example.bindingstudyapp.databinding.ActivityMainBinding
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
 
-        val button = findViewById<Button>(R.id.btnChangeText)
-        val textView = findViewById<TextView>(R.id.tvMessage)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        val mainText = textView.text
-        button.setOnClickListener {
-            button.isEnabled = false
+        val mainText = binding.tvMessage.text
+        binding.btnChangeText.setOnClickListener {
+            binding.btnChangeText.isEnabled = false
             lifecycleScope.launch {
-                textView.text = "Text changed with findViewById"
+                binding.tvMessage.text = "Text changed with View Binding"
                 delay(3000) // coroutines suspend function
-                textView.text = mainText
-                button.isEnabled = true
+                binding.tvMessage.text = mainText
+                binding.btnChangeText.isEnabled = true
             }
         }
     }
